d3910dd00c65d525129cf3f07b97ee6644b65900
PixelCraftLab <vishalgowdask65@gmail.com>
2026-04-01 19:55:23 +0530

    fetch added catogary wise

--- news.js
+++ news.js
@@ -1,6 +1,34 @@
-function input(){
-    const inp = document.getElementById("searchInput").value;
+// API_KEY=b5885e3890bd43e1918a1afeb8dec392
 
-    console.log(inp)
 
+
+const newsContainer = document.getElementById("newsContainer");
+
+async function details(category) {
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=b5885e3890bd43e1918a1afeb8dec392`
+    
+    const res = await fetch(url)
+    const data = await res.json()
+
+    displayNews(data.articles)
+}
+
+
+
+function displayNews(articles) {
+    newsContainer.innerHTML = ""
+
+    articles.forEach(article => {
+        const div = document.createElement("div")
+        div.classList.add("news-card")
+
+        div.innerHTML = `
+            <img src="${article.urlToImage}" />
+            <h3>${article.title}</h3>
+            <p>${article.description}</p>
+
+        `;
+
+        newsContainer.appendChild(div)
+    });
 }
